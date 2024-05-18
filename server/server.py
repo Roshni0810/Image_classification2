a79f43f3ade49f8647caf569ba8a503062071b81
Roshni0810 <roshni.chaurasia0810@gmail.com>
2024-05-18 18:17:47 +0530

    First ML Project

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/classify_image', methods=['GET]', 'POST'])
+def classify_image():
+    return "hi"
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
